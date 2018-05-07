7ea51a63331b9a5fc1f581498b64909a4a783896
victor chugbo <vcmails2010@gmail.com>
2018-05-07 07:34:39 +0100

    modified match

--- footy_lge_app/db/seed.rb
+++ footy_lge_app/db/seed.rb
@@ -6,11 +6,43 @@ require_relative('../models/team.rb')
 
 Team.delete_all()
 
-team1 = Team.new('name'=> 'Dukes', 'league'=> 'Ozzy1')
+team1 = Team.new("name"=> "Dukes", "league"=> "Ozzy1")
 team1.save()
 
-team2 = Team.new('name'=> 'Chiefs', 'league'=> 'Ozzy1')
+team2 = Team.new("name"=> "Chiefs", "league"=> "Ozzy1")
 team2.save()
 
-binding.pry
-nil
+
+#League.delete_all()
+
+league1 = League.new("name"=> "Ozzy1")
+league1.save()
+
+league2 = League.new("name"=> "Ozzy2")
+league2.save()
+
+
+#Match.delete_all()
+
+match1 = Match.new("league"=> "Ozzy1")
+match1.save()
+
+match2 = Match.new("league"=> "Ozzy2")
+match2.save()
+
+
+TeamMatch.delete_all()
+
+teamMatch1 = TeamMatch.new("match"=> "1",
+  "team1"=> "Gunners", "team2"=> "Targets",
+  "team1_stats_goals"=> "0", "team2team2_stats_goals"=> "2")
+  teamMatch1.save()
+
+  teamMatch2 = TeamMatch.new("match"=> "2",
+    "team1"=> "Guardians", "team2"=> "Timers",
+    "team1_stats_goals"=> "3", "team2team2_stats_goals"=> "1")
+    teamMatch2.save()
+
+
+    binding.pry
+    nil
